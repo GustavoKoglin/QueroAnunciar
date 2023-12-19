14a5fe77001370d504898b5021e0f60da6466a52
Gustavo Koglin <engcomputacao.gustavokoglin@gmail.com>
2023-12-18 22:51:11 -0400

    Inicio criação Form Anúncio

--- Front-End/src/app/formulario-anuncio/formulario-anuncio.component.ts
+++ Front-End/src/app/formulario-anuncio/formulario-anuncio.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-formulario-anuncio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgIf],
   templateUrl: './formulario-anuncio.component.html',
   styleUrl: './formulario-anuncio.component.scss'
 })
-export class FormularioAnuncioComponent {
 
+
+export class FormularioAnuncioComponent {
+  tipoAnuncio: string[] = ['imovel', 'veiculo', 'produto', 'servico'];
+  selectedTipoAnuncio: string = '';
+
+
+  // Adicione mais propriedades para os formulários específicos, se necessário
+
+  // Método para lidar com a mudança na seleção do tipo de anúncio
+  onTipoAnuncioChange() {
+    // Lógica para lidar com a mudança na seleção, se necessário
+  }
 }
